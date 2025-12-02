fa8eb1d5da458bc646ef6d10c53bdbaf922d9ba4
Carla Castro <carlacastro2997@gmail.com>
2025-12-02 04:27:57 -0300

    Se cambia la contraseña para db.py

--- backend/db.py
+++ backend/db.py
@@ -8,7 +8,7 @@ def get_connection():
     """
     # Obtener credenciales de variables de entorno o valores por defecto
     DB_USER = os.getenv("DB_USER", "root")
-    DB_PASS = os.getenv("DB_PASS", "tu_contraseña")
+    DB_PASS = os.getenv("DB_PASS", "")
     DB_HOST = os.getenv("DB_HOST", "localhost")
     DB_NAME = os.getenv("DB_NAME", "hotel_db")
 
